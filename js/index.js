2b58da407f165ba246849b1b1e1108f13869a00a
name <emonshah688@gmail.com>
2025-08-30 00:10:10 +0600

    Add/update website files: HTML, JS, CSS, Images

--- js/index.js
+++ js/index.js
@@ -38,3 +38,23 @@ window.onload=()=>{
     }
     
 }
+
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
