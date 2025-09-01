54e2adf56783f001ac43b5f63fe5d23c25169a92
MehediMihad <mehedimihad2004@gmail.com>
2025-09-01 15:46:47 +0600

    update javascript

--- js/index.js
+++ js/index.js
@@ -188,9 +188,32 @@ var swiper = new Swiper(".arrivals-slider", {
       delay: 3000,
       disableOnInteraction: false,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    
     breakpoints: {
       0: { slidesPerView: 1 },
       768: { slidesPerView: 2 },
       1024: { slidesPerView: 3 },
+    },
+  });
+
+   var swiper = new Swiper(".blogs-container", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
     },
   });
